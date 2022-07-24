64a482b22886fbf6110767b1c39d2af8840b39ac
so yeon <alicesysy@swu.ac.kr>
2022-07-24 23:30:18 +0900

    [#2] 이미지 에셋 추가

---
+++ Tamagotchi/Tamagotchi/Screen/Select/View/TamagotchiCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TamagotchiCollectionViewCell.swift
+//  Tamagotchi
+//
+//  Created by 소연 on 2022/07/24.
+//
+
+import UIKit
+
+class TamagotchiCollectionViewCell: UICollectionViewCell {
+    
+}
