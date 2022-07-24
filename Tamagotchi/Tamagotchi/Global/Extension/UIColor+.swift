2068e99f017982da95177f9f3f6d199c9166fc4a
so yeon <alicesysy@swu.ac.kr>
2022-07-24 21:00:25 +0900

    [#1] 색상 추가 및 세로방향 고정

---
+++ Tamagotchi/Tamagotchi/Global/Extension/UIColor+.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+.swift
+//  Tamagotchi
+//
+//  Created by 소연 on 2022/07/24.
+//
+
+import UIKit
+
+extension UIColor {
+    @nonobjc class var backgroundColor: UIColor {
+        return UIColor(red: 245.0 / 255.0, green: 252.0 / 255.0, blue: 252.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var foregroundColor: UIColor {
+        return UIColor(red: 77.0 / 255.0, green: 106.0 / 255.0, blue: 120.0 / 255.0, alpha: 1.0)
+    }
+}
