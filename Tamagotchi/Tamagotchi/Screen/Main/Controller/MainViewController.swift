f7d12ed06736f580a6782265fa9b0b1c40e25c91
so yeon <alicesysy@swu.ac.kr>
2022-07-24 21:05:49 +0900

    [#3] 메인 UI 관련 파일 추가

--- Tamagotchi/Tamagotchi/Screen/Main/Controller/MainViewController.swift
+++ Tamagotchi/Tamagotchi/Screen/Main/Controller/MainViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
 
+    private var userName: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setNavigationUI()
     }
-
-
+    
+    private func setNavigationUI() {
+        navigationItem.title = "\(userName)의 다마고치"
+        
+    }
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+    
 }
 
