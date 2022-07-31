be5ae039d64f66949eaa970ce7e2736e665c38b4
so yeon <alicesysy@swu.ac.kr>
2022-08-01 04:27:57 +0900

    [#8] 화면전환, alert 코드 메서드로 정리

--- Tamagotchi/Tamagotchi/Screen/Info/Controller/InfoTableViewController.swift
+++ Tamagotchi/Tamagotchi/Screen/Info/Controller/InfoTableViewController.swift
@@ -15,17 +15,21 @@ final class InfoTableViewController: UITableViewController {
     
     @IBOutlet weak var userNameLabel: UILabel!
     
+    @IBOutlet weak var switchLabel: UILabel!
+    @IBOutlet weak var switchButton: UISwitch!
+    
     // MARK: - Life Cycle
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        userNameLabel.text = "\(UserDefaults.standard.string(forKey: Constant.UserDefaults.UserName) ?? "대장")"
+        userNameLabel.text = UserDefaults.standard.string(forKey: Constant.UserDefaults.UserName) ?? "대장"
         configureNaivgationBarUI()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        configureLabel()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -46,10 +50,57 @@ final class InfoTableViewController: UITableViewController {
     
     private func configureTableView() {
         tableView.backgroundColor = .backgroundColor
-        
-        userNameLabel.text = "\(UserDefaults.standard.string(forKey: Constant.UserDefaults.UserName) ?? "대장")"
+    }
+    
+    private func configureLabel() {
+        userNameLabel.text = UserDefaults.standard.string(forKey: Constant.UserDefaults.UserName) ?? "대장"
         userNameLabel.textColor = .foregroundColor
         userNameLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        
+        switchLabel.text = "알림"
+        switchLabel.textColor = .black
+        switchLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        
+        switchButton.isOn = true
+    }
+    
+    private func showAlert() {
+        let alertController = UIAlertController(title: "데이터 초기화", message: "정말 다시 처음부터 시작하실 건가용?", preferredStyle: .alert)
+        
+        let cancelButton = UIAlertAction(title: "아냐!", style: .cancel)
+        let okayButton = UIAlertAction(title: "웅", style: .default) { _ in
+            UserDefaults.standard.removeObject(forKey: Constant.UserDefaults.TamagotchiName)
+            UserDefaults.standard.removeObject(forKey: Constant.UserDefaults.TamagotchiImageName)
+            UserDefaults.standard.removeObject(forKey: Constant.UserDefaults.UserName)
+            UserDefaults.standard.removeObject(forKey: Constant.UserDefaults.RiceCount)
+            UserDefaults.standard.removeObject(forKey: Constant.UserDefaults.WaterDropCount)
+            
+            let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+            let sceneDelegate = windowScene?.delegate as? SceneDelegate
+            
+            let storyBoard = UIStoryboard(name: Constant.Storyboard.Select, bundle: nil)
+            guard let viewController = storyBoard.instantiateViewController(withIdentifier: UINavigationViewController.identifier) as? UINavigationViewController else { return }
+            
+            sceneDelegate?.window?.rootViewController = viewController
+            sceneDelegate?.window?.makeKeyAndVisible()
+        }
+        
+        alertController.addAction(cancelButton)
+        alertController.addAction(okayButton)
+        
+        self.present(alertController, animated: true)
+    }
+    
+    private func pushToChangeUserName() {
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: UserNameViewController.identifier) as? UserNameViewController else { return }
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func pushToChangeTamagotchi() {
+        let storyBoard = UIStoryboard(name: Constant.Storyboard.Select, bundle: nil)
+        guard let viewController = storyBoard.instantiateViewController(withIdentifier: SelectCollectionViewController.identifier) as? SelectCollectionViewController else { return }
+        viewController.viewType = .channge
+        self.navigationController?.pushViewController(viewController, animated: true)
     }
 }
 
@@ -64,40 +115,13 @@ extension InfoTableViewController {
         switch indexPath.row {
         /// 내 이름 설정하기
         case 0:
-            guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: UserNameViewController.identifier) as? UserNameViewController else { return }
-            self.navigationController?.pushViewController(viewController, animated: true)
+            pushToChangeUserName()
         /// 다마고치 변경하기
         case 1:
-            let storyBoard = UIStoryboard(name: Constant.Storyboard.Select, bundle: nil)
-            guard let viewController = storyBoard.instantiateViewController(withIdentifier: SelectCollectionViewController.identifier) as? SelectCollectionViewController else { return }
-            viewController.viewType = .channge
-            self.navigationController?.pushViewController(viewController, animated: true)
+            pushToChangeTamagotchi()
         /// 데이터 초기화
         case 2:
-            let alertController = UIAlertController(title: "데이터 초기화", message: "정말 다시 처음부터 시작하실 건가용?", preferredStyle: .alert)
-            
-            let cancelButton = UIAlertAction(title: "아냐!", style: .cancel)
-            let okayButton = UIAlertAction(title: "웅", style: .default) { _ in
-                UserDefaults.standard.removeObject(forKey: Constant.UserDefaults.TamagotchiName)
-                UserDefaults.standard.removeObject(forKey: Constant.UserDefaults.TamagotchiImageName)
-                UserDefaults.standard.removeObject(forKey: Constant.UserDefaults.UserName)
-                UserDefaults.standard.removeObject(forKey: Constant.UserDefaults.RiceCount)
-                UserDefaults.standard.removeObject(forKey: Constant.UserDefaults.WaterDropCount)
-                
-                let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-                let sceneDelegate = windowScene?.delegate as? SceneDelegate
-                
-                let storyBoard = UIStoryboard(name: Constant.Storyboard.Select, bundle: nil)
-                guard let viewController = storyBoard.instantiateViewController(withIdentifier: UINavigationViewController.identifier) as? UINavigationViewController else { return }
-                
-                sceneDelegate?.window?.rootViewController = viewController
-                sceneDelegate?.window?.makeKeyAndVisible()
-            }
-            
-            alertController.addAction(cancelButton)
-            alertController.addAction(okayButton)
-            
-            self.present(alertController, animated: true)
+            showAlert()
         default:
             return
         }
