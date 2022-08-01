d7d0ea54931b904bd972efbecb3ffc83ee4e7896
so yeon <alicesysy@swu.ac.kr>
2022-08-02 02:25:29 +0900

    [#8] 유저이름 기본값 상수화

---
+++ Tamagotchi/Tamagotchi/Global/Constant/User.swift
@@ -0,0 +1,14 @@
+//
+//  String.swift
+//  Tamagotchi
+//
+//  Created by 소연 on 2022/08/02.
+//
+
+import Foundation
+
+extension Constant {
+    struct UserName {
+        static let UserName: String = "대장"
+    }
+}
